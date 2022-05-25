ece9c4edb195e6f70ab8205031ce1c475079cb68
hehu <hehu@hehu.fun>
2022-05-25 15:01:21 +0800

    1.06
    百度统计功能

---
+++ assect/js/count.js
@@ -0,0 +1,10 @@
+
+// 百度统计
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?49b8bf2e4930ba5c0e7f910453829ab5";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
